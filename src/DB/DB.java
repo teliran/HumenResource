60e1a719e14230b0d8ce934014aa29370f7f778b
YakirZana <yaqir11@gmail.com>
2016-04-07 21:44:53 +0300

    Implement basic manu on Store Implement functions on DB , with handled Exceptions

---
+++ src/DB/DB.java
@@ -0,0 +1,104 @@
+package DB;
+
+import java.sql.*;
+
+public final class DB
+{
+	private static Connection connection;
+	private static Statement db;
+	
+	private DB(){
+		connection=null;
+		db=null;
+	}
+	
+	public static ResultSet executeQuery(String query){
+		try {
+			return db.executeQuery(query);
+		} catch (SQLException e) {
+			System.out.println("Problem with reading from database");
+			e.printStackTrace();
+			System.exit(0);
+			return null;
+		}	
+	}
+	
+	public static boolean executeUpdate(String query){
+		try {
+			db.executeUpdate(query);
+			return true;
+		} catch (SQLException e) {
+			System.out.println("Problem with Edit database");
+			e.printStackTrace();
+			return false;
+		}	
+	}
+	
+	public static boolean next(ResultSet set){
+		try {
+			return set.next();
+		} catch (SQLException e) {
+			System.out.println("Problem with reading from database");
+			e.printStackTrace();
+			System.exit(0);
+			return false;
+		}
+	}
+	
+	public static int getInt(ResultSet set,String id){
+		try {
+			return set.getInt(id);
+		} catch (SQLException e) {
+			System.out.println("Problem with reading from database");
+			e.printStackTrace();
+			System.exit(0);
+			return 0;
+		}
+	}
+	
+	public static String getString(ResultSet set,String id){
+		try {
+			return set.getString(id);
+		} catch (SQLException e) {
+			System.out.println("Problem with reading from database");
+			e.printStackTrace();
+			System.exit(0);
+			return "";
+		}
+	}
+	
+	public static void closeResult(ResultSet set){
+		try {
+			set.close();
+		} catch (SQLException e) {
+			System.out.println("Problem with reading from database");
+			e.printStackTrace();
+			System.exit(0);
+		}		
+	}
+	
+	public static void open(){
+		try{
+			Class.forName("org.sqlite.JDBC");
+			connection = DriverManager.getConnection("jdbc:sqlite:main.db");
+			connection.setAutoCommit(true);
+			db = connection.createStatement();
+		} catch (Exception e)  {
+			System.out.println("Error on Loading Database");
+			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+			System.exit(0);
+	    }
+	}
+	
+	public static void close(){
+		try {
+			db.close();
+			connection.close();
+		} catch (SQLException e) {
+			System.out.println("Error on Clossing Database");
+			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+			System.exit(0);
+		}
+	}
+
+}
--- src/Emp/Store.java
+++ src/Emp/Store.java
@@ -1,10 +1,89 @@
 package Emp;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+import DB.DB;
+
 public class Store {
+	private String name;
+	private String password;
+	
+	public Store(String name, String password){
+		this.name=name;
+		this.password=password;
+	}
+	
+	public void showManu(){	
+		int userInput;
+		System.out.println("Welcome to "+name+"!");
+		login();
+		while (true){
+			System.out.println("Manu :");
+			System.out.println("1.\t Employees");
+			System.out.println("2.\t Employees");
+			System.out.println("3.\t Exit");	
+			userInput = getNumber();
+			switch (userInput){
+			case 1:{			
+				ResultSet rs = DB.executeQuery( "SELECT * FROM Employees;" );
+				while (DB.next(rs)) {
+					int id = DB.getInt(rs,"ID");
+					String  name = DB.getString(rs,"Name");
+					System.out.println( "ID = " + id );
+					System.out.println( "NAME = " + name );
+					System.out.println();
+				}
+				DB.closeResult(rs);	
+				break;
+			}
+			case 3: //EXIT
+				return;
+			
+			}
+		}	
+	}
+	
+	public void login(){
+		Scanner sc = new Scanner(System.in);
+		while(true){
+			System.out.println("Please Enter The password :");		
+			String password = sc.nextLine();		
+			if(password.equals(this.password)){
+				System.out.println("Great! log in ...");
+				return;
+			}
+			System.out.println("Wrong password! , Please try again");		
+		}	
+	}
+	
+	public static int getNumber(){
+		Scanner sc = new Scanner(System.in);
+		String userNum;
+		while(true){
+			userNum = sc.nextLine();
+			if(isNumber(userNum)){
+				return Integer.parseInt(userNum);
+			}
+			System.out.println("Invalid number , Please try again");
+		}	
+	}
+	
+	public static boolean isNumber(String number){
+		try{
+			Integer.parseInt(number);
+		}
+		catch(Exception c){
+			return false;
+		}
+		return true;
+	}
 
 	public static void main(String[] args) {
-		System.out.println("Hallo Smliran's");
-
+		DB.open();
+		Store store = new Store("Smliran's Store","1234");
+		store.showManu();
+		DB.close();
 	}
 
 }
