d26d1962423945ba85de54c69a37b60a27424926
YakirZana <yaqir11@gmail.com>
2016-04-07 19:40:37 +0300

    Add DB classes

---
+++ src/DB/Db.java
@@ -0,0 +1,23 @@
+package DB;
+
+import java.sql.*;
+
+public class Db
+{
+	public static void main( String args[] )
+	  {
+	    Connection c = null;
+	    Statement stmt = null;
+	    try {
+	      Class.forName("org.sqlite.JDBC");
+	      c = DriverManager.getConnection("jdbc:sqlite:main.db");
+	      c.setAutoCommit(true);
+	      System.out.println("Opened database successfully");
+	      c.close();
+	    } catch ( Exception e ) {
+	      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	      System.exit(0);
+	    }
+	    System.out.println("Operation done successfully");
+	  }
+}
